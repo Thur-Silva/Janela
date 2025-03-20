c4406fd3c671c04744ae1624fc121d5fce044f3a
Arthur <arthurcaue100@gmail.com>
2025-03-19 21:28:40 -0300

    Criando um Forms reativo personalisado.

--- Janela/Form1.Designer.cs
+++ Janela/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Minimizar = new System.Windows.Forms.PictureBox();
+            this.Maximizar = new System.Windows.Forms.PictureBox();
+            this.Fechar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Maximizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fechar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.Fechar);
+            this.panel1.Controls.Add(this.Maximizar);
+            this.panel1.Controls.Add(this.Minimizar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 52);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // Minimizar
+            // 
+            this.Minimizar.BackgroundImage = global::Janela.Properties.Resources.minimizar_icon;
+            this.Minimizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Minimizar.Location = new System.Drawing.Point(653, 12);
+            this.Minimizar.Name = "Minimizar";
+            this.Minimizar.Size = new System.Drawing.Size(33, 33);
+            this.Minimizar.TabIndex = 3;
+            this.Minimizar.TabStop = false;
+            this.Minimizar.Click += new System.EventHandler(this.Minimizar_Click);
+            this.Minimizar.MouseHover += new System.EventHandler(this.Minimizar_MouseHover);
+            // 
+            // Maximizar
+            // 
+            this.Maximizar.BackgroundImage = global::Janela.Properties.Resources.maximizar_icon;
+            this.Maximizar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Maximizar.Location = new System.Drawing.Point(704, 12);
+            this.Maximizar.Name = "Maximizar";
+            this.Maximizar.Size = new System.Drawing.Size(33, 33);
+            this.Maximizar.TabIndex = 2;
+            this.Maximizar.TabStop = false;
+            this.Maximizar.Click += new System.EventHandler(this.Maximizar_Click);
+            this.Maximizar.MouseLeave += new System.EventHandler(this.Maximizar_MouseLeave);
+            this.Maximizar.MouseHover += new System.EventHandler(this.Maximizar_MouseHover);
+            // 
+            // Fechar
+            // 
+            this.Fechar.BackgroundImage = global::Janela.Properties.Resources.fechar_icon;
+            this.Fechar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Fechar.Location = new System.Drawing.Point(755, 12);
+            this.Fechar.Name = "Fechar";
+            this.Fechar.Size = new System.Drawing.Size(33, 33);
+            this.Fechar.TabIndex = 1;
+            this.Fechar.TabStop = false;
+            this.Fechar.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.Fechar.MouseLeave += new System.EventHandler(this.Fechar_MouseLeave);
+            this.Fechar.MouseHover += new System.EventHandler(this.Fechar_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Minimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Maximizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Fechar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Fechar;
+        private System.Windows.Forms.PictureBox Maximizar;
+        private System.Windows.Forms.PictureBox Minimizar;
     }
 }
 
--- Janela/Form1.cs
+++ Janela/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,72 @@ namespace Janela
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Maximizar_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                
+                this.WindowState = FormWindowState.Maximized;
+                Minimizar.Location = new Point(1700, 12);
+                Maximizar.Location = new Point(1751, 12);   
+                Fechar.Location = new Point(1802, 12);  
+            }
+        }
+
+        private void Minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Fechar_MouseHover(object sender, EventArgs e)
+        {
+            Fechar.Size = new Size(35, 35);
+        }
+
+        private void Fechar_MouseLeave(object sender, EventArgs e)
+        {
+            Fechar.Size = new Size(33, 33);
+        }
+
+        private void Maximizar_MouseHover(object sender, EventArgs e)
+        {
+            Maximizar.Size = new Size(35, 35);
+        }
+
+        private void Maximizar_MouseLeave(object sender, EventArgs e)
+        {
+            Maximizar.Size = new Size(33, 33);
+        }
+
+        private void Minimizar_MouseHover(object sender, EventArgs e)
+        {
+            Minimizar.Size = new Size(35, 35);
+        }
+
+        private void Minimizar_MouseLeave(object sender, EventArgs e)
+        {
+            Minimizar.Size = new Size(33, 33);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        [DllImport("user32.DLL",EntryPoint="ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL",EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
     }
 }
